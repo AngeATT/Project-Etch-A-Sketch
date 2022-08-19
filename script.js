4a238da29b48826f9a11dcaffb996f8a981f4e80
AngeATT <96740251+AngeATT@users.noreply.github.com>
2022-08-19 11:39:04 +0000

    Ajout reinitialisation case

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const body = document.querySelector("#body");
 const square_divs = document.querySelector("#square-divs");
 const sizeSlider = document.querySelector('#sizeSlider');
-const clearBoutton = document.querySelector('#clearBoutton');
+const reinitialiser_tableau = document.querySelector('#reinitialiserTableau');
 const carre = document.querySelector;
 const sizeValue = document.querySelector('#sizeValue');
 const default_click_color = 'white';
 const default_color = "rgb(73, 100, 122)";
 
-const reinitialiser_case = document.querySelector("#clearBoutton");
+const mode_rainbow = document.querySelector('#mode_rainbow');
+
+const delete_couleur = document.querySelector("#deleteColor");
+
+
+
 
 let default_size = 16;
 let tableau_de_divs = [];
@@ -19,7 +24,7 @@ function initialise(entry_number){
     var square_size = 480/entry_number;
     for ( let j = 0 ; j < square_numbers ; j++ ){
       tableau_de_divs[j] = document.createElement("div");
-
+      
       tableau_de_divs[j].classList.add( 'carre' );
 
       tableau_de_divs[j].style['height'] = `${square_size}px`;
@@ -32,25 +37,49 @@ function initialise(entry_number){
 }
 
 function color_cases(color){
+  
   tableau_de_divs.forEach(element => {
+  let couleur = color;
   element.addEventListener(
     'click',() =>{
-      element.style.removeProperty('background-color');
-      }
+      console.log(couleur);
+      element.style['background-color'] = couleur;}
                           )})
 }
+
+
+
+
 
 initialise(default_size);
 color_cases(default_click_color);
 
-clearBoutton.addEventListener('click',() => {
+reinitialiser_tableau.addEventListener('click',() => {
 sizeSlider.value = 16;
-initialise(default_size);
+initialise(16);
+color_cases(default_click_color);
 
 });
 sizeSlider.addEventListener('input', () => {
   initialise(sizeSlider.value);
+  color_cases(default_click_color);
 }) ;
+
+delete_couleur.addEventListener('click',
+() => color_cases( default_color ) );
+
+function rgb_generator() {
+  let value_1= Math.floor(Math.random() * 255);
+  let value_2= Math.floor(Math.random() * 255);
+  let value_3= Math.floor(Math.random() * 255);
+
+  return `rgb(${value_1},${value_2},${value_3})`
+}
+
+mode_rainbow.addEventListener('click',() => {
+initialise();
+color_cases(rgb_generator());}
+)
 
 
 
